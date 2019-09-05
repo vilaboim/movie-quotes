a38b0ac83620317544a3ea14383792394d3651c0
Lucas Vilaboim <lucas@vilaboim.com>
2019-09-05 19:02:46 -0300

    fix(cli): update meow syntax

--- cli.js
+++ cli.js
@@ -3,18 +3,18 @@
 var meow = require('meow');
 var movieQuotes = require('./');
 
-var cli = meow([
-	'Examples',
-	'  $ movie-quotes',
-	'  "Show me the money!" Jerry Maguire',
-	'',
-	'  $ movie-quotes --all',
-	'  "Frankly, my dear, I don\'t give a damn." Gone with the Wind',
-	'  "I\'m gonna make him an offer he can\'t refuse." The Godfather',
-	'  ...',
-	'',
-	'Options',
-	'  --all   Get all quotes instead of a random quote'
-]);
+var cli = meow(`
+	Examples
+	 $ movie-quotes
+	 "Show me the money!" Jerry Maguire
+
+	 $ movie-quotes --all',
+	 "Frankly, my dear, I don\'t give a damn." Gone with the Wind
+	 "I\'m gonna make him an offer he can\'t refuse." The Godfather
+	 ...
+
+	Options
+	 --all   Get all quotes instead of a random quote
+`);
 
 console.log(cli.flags.all ? movieQuotes.all.join('\n') : movieQuotes.random());
